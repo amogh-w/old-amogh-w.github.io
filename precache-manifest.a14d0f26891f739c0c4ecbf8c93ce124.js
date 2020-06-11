d65c486eeb523f699ce102879f4a4d815c2ff835
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-06-11 19:09:39 +0530

    Updates

---
+++ precache-manifest.a14d0f26891f739c0c4ecbf8c93ce124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d403156eafa3551d94743fa07591af2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d66c35ed4615e62a4860",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "713c4c7919be28f4622b",
+    "url": "/static/js/2.2fa1c3be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2fa1c3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66c35ed4615e62a4860",
+    "url": "/static/js/main.a28587ab.chunk.js"
+  },
+  {
+    "revision": "bdb309e6bdcfe5efc4f1",
+    "url": "/static/js/runtime-main.4f4ffe5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
