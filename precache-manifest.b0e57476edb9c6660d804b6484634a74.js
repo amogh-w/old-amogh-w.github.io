3be9cd3e525e403f75b4ab919d537cf06027f57c
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-06-22 13:32:54 +0530

    Updates

--- precache-manifest.b0e57476edb9c6660d804b6484634a74.js
+++ precache-manifest.b0e57476edb9c6660d804b6484634a74.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1d4cc27d6ef828a89aab8a69ddcf553",
+    "revision": "07d7207f8c8c70f42943677ccb6ec832",
     "url": "/index.html"
   },
   {
-    "revision": "8d0b1710daab236853ef",
-    "url": "/static/js/2.5e16aca8.chunk.js"
+    "revision": "5ce508f2a171224517cf",
+    "url": "/static/js/2.00a1f1e8.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.5e16aca8.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.00a1f1e8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "50497403ceb727fc9659",
-    "url": "/static/js/main.6862fa47.chunk.js"
+    "revision": "f4623f2924f19c0db3c6",
+    "url": "/static/js/main.10f3ca53.chunk.js"
   },
   {
     "revision": "bdb309e6bdcfe5efc4f1",
